f50f615b7ef06b7eb47c9d990dd57b326cb112c9
unfl1 <flxmaut10@gmail.com>
2025-05-09 23:16:34 +0900

    유저 정보 추가

--- src/main/java/com/hj/review/domain/User.java
+++ src/main/java/com/hj/review/domain/User.java
@@ -16,4 +16,13 @@ public class User {
 
     private String provider;     // ex) kakao
     private String providerId;   // 카카오에서 받은 유저 ID
+
+    private String nickname;     // 사용자 닉네임
+    private String email;        // 사용자 이메일
+
+    // 추가 정보 수정 메서드
+    public void updateProfile(String nickname, String email) {
+        this.nickname = nickname;
+        this.email = email;
+    }
 }
