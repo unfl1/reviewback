16ca9d2eaef66d85592e9aca5311ee7fe799af44
unfl1 <flxmaut10@gmail.com>
2025-08-01 22:38:40 +0900

    Board 도메인 생성

--- src/main/java/com/hj/review/domain/Board.java
+++ src/main/java/com/hj/review/domain/Board.java
@@ -1,4 +1,16 @@
 package com.hj.review.domain;
 
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Board {
+    private long id;
+    private String title;
+    private String content;
 }
